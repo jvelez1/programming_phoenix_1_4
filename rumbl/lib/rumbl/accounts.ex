2f4686bb5ca4e9b680789b083f3e2faaf9454151
josue <josuevelezdr@gmail.com>
2021-04-25 12:08:18 +0200

    [Chapter 4] Ecto + create action

--- rumbl/lib/rumbl/accounts.ex
+++ rumbl/lib/rumbl/accounts.ex
@@ -2,27 +2,32 @@ defmodule Rumbl.Accounts do
   @moduledoc """
     The account context
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
   def list_users do
-    [
-      %User{id: "1", name: "Josue", username: "velecito"},
-      %User{id: "2", name: "Eduardo", username: "osito"},
-      %User{id: "3", name: "Gustavo", username: "galaretisha"}
-    ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(),
-      fn map -> map.id == id end
-    )
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params,
-      fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
   end
 end
