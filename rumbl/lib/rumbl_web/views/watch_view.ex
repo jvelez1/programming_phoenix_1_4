8d1ceb508bac513fb1bd281def079c72a5f24315
josue <josuevelezdr@gmail.com>
2021-05-02 21:14:05 +0200

    [Chapter 9] Adding javascript for reproducing videos

--- rumbl/lib/rumbl_web/views/watch_view.ex
+++ rumbl/lib/rumbl_web/views/watch_view.ex
@@ -2,7 +2,7 @@ defmodule RumblWeb.WatchView do
   use RumblWeb, :view
 
   def player_id(video) do
-    ~r{^.*(​?:​youtu\.be/|\w+/|v=)(​?<​id>[^​#&?]*)}
+    ~r{^.*(?:youtu\.be/|\w+/|v=)(?<id>[^#&?]*)}
     |> Regex.named_captures(video.url)
     |> get_in(["id"])
   end
