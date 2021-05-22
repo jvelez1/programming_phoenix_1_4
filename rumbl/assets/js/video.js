7a7fb7d9165e3fd82f2ce1174decf05d62276b82
Josue Velez <josuevelezdr@gmail.com>
2021-05-22 21:47:54 +0200

    [Chapter 10] adding Annotations model and persist annotations with socket

--- rumbl/assets/js/video.js
+++ rumbl/assets/js/video.js
@@ -19,36 +19,73 @@ let Video = {
 
     postButton.addEventListener("click", e => {
       let payload = {body: msgInput.value, at: Player.getCurrentTime()}
-      vidChannel.push("new_annotation", payload)        
-                .receive("error", e => console.log(e) ) 
+      vidChannel.push("new_annotation", payload)
+                .receive("error", e => console.log(e) )
       msgInput.value = ""
     })
 
-    vidChannel.on("new_annotation", (resp) => {         
+    msgContainer.addEventListener("click", e => {
+      e.preventDefault()
+      let seconds = e.target.getAttribute("data-seek") ||
+                    e.target.parentNode.getAttribute("data-seek")
+      if(!seconds){ return }
+
+      Player.seekTo(seconds)
+    })
+
+    vidChannel.on("new_annotation", (resp) => {
       this.renderAnnotation(msgContainer, resp)
     })
 
     vidChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp) )
+      .receive("ok", resp => {
+        this.scheduleMessages(msgContainer, resp.annotations)
+      })
       .receive("error", reason => console.log("join failed", reason) )
   },
 
-  esc(str){ 
-    let div = document.createElement("div")
-    div.appendChild(document.createTextNode(str))
-    return div.innerHTML
-  },
-
-  renderAnnotation(msgContainer, {user, body, at}){ 
+  renderAnnotation(msgContainer, {user, body, at}){
     let template = document.createElement("div")
-
     template.innerHTML = `
     <a href="#" data-seek="${this.esc(at)}">
+      [${this.formatTime(at)}]
       <b>${this.esc(user.username)}</b>: ${this.esc(body)}
     </a>
     `
     msgContainer.appendChild(template)
     msgContainer.scrollTop = msgContainer.scrollHeight
+  },
+
+  scheduleMessages(msgContainer, annotations){
+    clearTimeout(this.scheduleTimer)
+    this.schedulerTimer = setTimeout(() => {
+      let ctime = Player.getCurrentTime()
+      let remaining = this.renderAtTime(annotations, ctime, msgContainer)
+      this.scheduleMessages(msgContainer, remaining)
+    }, 1000)
+  },
+
+  renderAtTime(annotations, seconds, msgContainer){
+    return annotations.filter( ann => {
+      if(ann.at > seconds){
+        return true
+      } else {
+        this.renderAnnotation(msgContainer, ann)
+        return false
+      }
+    })
+  },
+
+  formatTime(at){
+    let date = new Date(null)
+    date.setSeconds(at / 1000)
+    return date.toISOString().substr(14, 5)
+  },
+
+  esc(str){
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
   }
 }
 export default Video
--- rumbl/lib/rumbl/multimedia.ex
+++ rumbl/lib/rumbl/multimedia.ex
@@ -9,6 +9,7 @@ defmodule Rumbl.Multimedia do
 
   alias Rumbl.Multimedia.Category
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.Annotation
 
   @doc """
   Returns the list of videos.
@@ -125,6 +126,21 @@ defmodule Rumbl.Multimedia do
     Category
     |> Category.alphabetical
     |> Repo.all()
+  end
+
+  def annotate_video(%Accounts.User{id: user_id}, video_id, attrs) do
+    %Annotation{video_id: video_id, user_id: user_id}
+    |> Annotation.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def list_annotations(%Video{} = video) do
+    Repo.all(
+      from a in Ecto.assoc(video, :annotations),
+      order_by: [asc: a.at, asc: a.id],
+      limit: 500,
+      preload: [:user]
+    )
   end
 
   defp user_videos_query(query, %Accounts.User{id: user_id}) do
