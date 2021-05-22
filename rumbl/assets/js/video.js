f1d84e524a50ec9370b1bea6b8f153d8e644e7cb
Josue Velez <josuevelezdr@gmail.com>
2021-05-22 20:18:56 +0200

    [Chapter 10] Adding handle_in callback for fetching messages from text box

--- rumbl/assets/js/video.js
+++ rumbl/assets/js/video.js
@@ -6,7 +6,7 @@ let Video = {
     let playerId = element.getAttribute("data-player-id")
     let videoId  = element.getAttribute("data-id")
     socket.connect()
-    Player.init(element.id, playerId, () => { 
+    Player.init(element.id, playerId, () => {
       this.onReady(videoId, socket)
     })
   },
@@ -17,9 +17,38 @@ let Video = {
     let postButton   = document.getElementById("msg-submit")
     let vidChannel   = socket.channel("videos:" + videoId)
 
+    postButton.addEventListener("click", e => {
+      let payload = {body: msgInput.value, at: Player.getCurrentTime()}
+      vidChannel.push("new_annotation", payload)        
+                .receive("error", e => console.log(e) ) 
+      msgInput.value = ""
+    })
+
+    vidChannel.on("new_annotation", (resp) => {         
+      this.renderAnnotation(msgContainer, resp)
+    })
+
     vidChannel.join()
-      .receive("ok", resp => console.log("joined the video channel", resp))
-      .receive("error", reason => console.log("join failed", reason))
+      .receive("ok", resp => console.log("joined the video channel", resp) )
+      .receive("error", reason => console.log("join failed", reason) )
+  },
+
+  esc(str){ 
+    let div = document.createElement("div")
+    div.appendChild(document.createTextNode(str))
+    return div.innerHTML
+  },
+
+  renderAnnotation(msgContainer, {user, body, at}){ 
+    let template = document.createElement("div")
+
+    template.innerHTML = `
+    <a href="#" data-seek="${this.esc(at)}">
+      <b>${this.esc(user.username)}</b>: ${this.esc(body)}
+    </a>
+    `
+    msgContainer.appendChild(template)
+    msgContainer.scrollTop = msgContainer.scrollHeight
   }
 }
 export default Video
--- rumbl/lib/rumbl_web/channels/video_channel.ex
+++ rumbl/lib/rumbl_web/channels/video_channel.ex
@@ -4,4 +4,15 @@ defmodule RumblWeb.VideoChannel do
   def join("videos:" <> video_id, _params, socket) do
     {:ok, assign(socket, :video_id, String.to_integer(video_id))}
   end
+
+  # “Forwarding a raw message payload without inspection is a big security risk.”
+  def handle_in("new_annotation", params, socket) do
+    broadcast!(socket, "new_annotation", %{
+      user: %{username: "anon"},
+      body: params["body"],
+      at: params["at"]
+    })
+
+    {:reply, :ok, socket}
+  end
 end
