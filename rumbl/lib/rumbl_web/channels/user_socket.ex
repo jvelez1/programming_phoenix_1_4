cf29834ca6ad1800d2139010ba7b6c9f33db4e68
Josue Velez <josuevelezdr@gmail.com>
2021-05-22 20:55:14 +0200

    [Chapter 10] adding Authentication for web-socket connection

--- rumbl/lib/rumbl_web/channels/user_socket.ex
+++ rumbl/lib/rumbl_web/channels/user_socket.ex
@@ -1,6 +1,7 @@
 defmodule RumblWeb.UserSocket do
   use Phoenix.Socket
 
+  @max_age 2 * 7 * 24 * 60 * 60
   ## Channels
   # channel "room:*", RumblWeb.RoomChannel
 
@@ -17,9 +18,17 @@ defmodule RumblWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, _reason} ->
+        :error
+    end
   end
+
+  def connect(_params, _, _), do: :error
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
@@ -30,6 +39,9 @@ defmodule RumblWeb.UserSocket do
   #
   #     RumblWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
   #
+
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
+
   # Returning `nil` makes this socket anonymous.
   def id(_socket), do: nil
 end
